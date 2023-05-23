6a77f01e3ba41a5fee58913ead004a160fc4c0fd
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-05-22 18:15:46 -0700

    removed mininmum reaction length constrain

--- models/Reaction.js
+++ models/Reaction.js
@@ -10,7 +10,6 @@ const reactionSchema = new Schema(
         reactionBody: {
             type: String,
             required: true,
-            minlength: 1,
             maxlength: 280,
 
         },
